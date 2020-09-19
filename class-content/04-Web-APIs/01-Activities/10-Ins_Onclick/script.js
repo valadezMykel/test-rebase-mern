eb5e197d86f2a63a9bd49e695cefab93f416e5d1
John Young <john@codeandcreative.com>
2020-09-19 09:23:56 -0700

    Adds edits to some of the activities, update to a form submissions slideshow

--- class-content/04-Web-APIs/01-Activities/10-Ins_Onclick/script.js
+++ class-content/04-Web-APIs/01-Activities/10-Ins_Onclick/script.js
@@ -1,13 +1,17 @@
-var themeSwitcher = document.querySelector("#theme-switcher");
-var container = document.querySelector(".container");
+// Step 1: get reference to UI elements
+var themeSwitcher = document.querySelector("#theme-switcher"); // the clickable UI element
+var container = document.querySelector(".container"); // the UI element updated by the click
 var mode = "dark";
 
-themeSwitcher.addEventListener("click", function() {
+// Step 2: listen for user interaction
+themeSwitcher.addEventListener("click", function () {
+  // Step 3: handle the user interaction
+
+  // Step 4: update the UI
   if (mode === "dark") {
     mode = "light";
     container.setAttribute("class", "light");
-  }
-  else {
+  } else {
     mode = "dark";
     container.setAttribute("class", "dark");
   }
