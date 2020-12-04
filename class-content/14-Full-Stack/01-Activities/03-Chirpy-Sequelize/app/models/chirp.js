ba24de1223a67465e410fdcb2804c1b993214f91
John Young <john@codeandcreative.com>
2020-12-03 17:30:48 -0800

    Modify week 14 activity 3

--- class-content/14-Full-Stack/01-Activities/03-Chirpy-Sequelize/app/models/chirp.js
+++ class-content/14-Full-Stack/01-Activities/03-Chirpy-Sequelize/app/models/chirp.js
@@ -3,11 +3,11 @@
 
 // This may be confusing but here Sequelize (capital) references the standard library
 var Sequelize = require("sequelize");
-// sequelize (lowercase) references our connection to the DB.
-var sequelize = require("../config/connection.js");
+// sequelizeConnection (lowercase) references our connection to the DB.
+var sequelizeConnection = require("../config/connection.js");
 
 // Creates a "Chirp" model that matches up with DB
-var Chirp = sequelize.define("chirp", {
+var Chirp = sequelizeConnection.define("chirp", {
   author: Sequelize.STRING,
   body: Sequelize.STRING,
   created_at: Sequelize.DATE
